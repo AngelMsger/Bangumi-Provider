bb8eea3c33b6c8755a246c4d735181a927d13422
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-02-28 00:54:08 +0800

    Fix Fatal Bugs.

--- exec.py
+++ exec.py
@@ -179,7 +179,7 @@ class BangumiCrawler:
 
 
 if __name__ == '__main__':
-    print('[INFO] Hello! This is Bangumi-Crawler :)\n[INFO] Task Schedules at $s Everyday.' % conf.CRON_AT)
+    print('[INFO] Hello! This is Bangumi-Crawler :)\n[INFO] Task Schedules at %s Everyday.' % conf.CRON_AT)
 
     crawler = BangumiCrawler(MongoDB, conf)
     schedule.every().day.at(conf.CRON_AT).do(crawler.crawl)
