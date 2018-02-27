5b87e798dd44518c244f32c1908a92637877a42f
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-02-27 23:11:31 +0800

    Modify Deploy Behavior.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-docker push angelmsger/bangumi-crawler:${TRAVIS_TAG:-latest}
+# Login Registry(Docker Hub)
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+# Generate Image Author/Name/Tag
+image=angelmsger/bangumi-crawler:${TRAVIS_TAG:-latest}
+
+# Tag Image
+docker tag bangumi-crawler $image
+
+# Push to Registry(Docker Hub)
+docker push $image
+
+# Request Webhook
+# curl https://api.angelmsger.com/code?image=$image || /bin/true
