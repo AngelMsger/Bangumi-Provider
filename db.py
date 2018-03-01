3ab568e731b8d13891e3cce8f795d42d957f3e2e
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-03-01 16:10:42 +0800

    Fix MongoDB Bugs.

--- db.py
+++ db.py
@@ -79,5 +79,5 @@ class MongoDB(DB):
 
     def __init__(self, conf) -> None:
         self.db = MongoClient(conf.DB_HOST, conf.DB_PORT)[conf.DB_DATABASE]
-        if conf.DB_MONGO_ENABLE_AUTH:
+        if conf.DB_ENABLE_AUTH:
             self.db.authenticate(conf.DB_USERNAME, conf.DB_PASSWORD)
